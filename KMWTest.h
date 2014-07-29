98ceca67202f04f25faddb860c348ab283f0e742
KAI MING WONG <kaimingwong@KAIs-MacBook-Air.local>
2014-07-30 01:13:40 +0800

    added a test class

---
+++ KMWTest.h
@@ -0,0 +1,13 @@
+//
+//  KMWTest.h
+//  Git Test
+//
+//  Created by KAI MING WONG on 30/7/14.
+//  Copyright (c) 2014 KMW. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface KMWTest : NSObject
+
+@end
---
+++ KMWTest.m
@@ -0,0 +1,13 @@
+//
+//  KMWTest.m
+//  Git Test
+//
+//  Created by KAI MING WONG on 30/7/14.
+//  Copyright (c) 2014 KMW. All rights reserved.
+//
+
+#import "KMWTest.h"
+
+@implementation KMWTest
+
+@end
